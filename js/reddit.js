dfd4fc57b2616c330098d1300fdb52677e08b076
Cheese <33181649+adhesivecheese@users.noreply.github.com>
2024-06-30 22:04:40 +0000

    .

--- js/reddit.js
+++ js/reddit.js
@@ -25,8 +25,8 @@ class Reddit {
     get_endpoint(endpoint) {
         let endpoints = {
             me: "api/v1/me"
-            , messages: "message/messages"
-            , sent: "message/sent"
+            , messages: "message/messages?limit=100&raw_json=1"
+            , sent: "message/sent?limit=100&raw_json=1"
             , unread: "message/unread"
             , comment_replies: "message/comments"
             , post_replies: "message/selfreply"
@@ -84,18 +84,16 @@ class Reddit {
     }
 
     async initialFetch() {
-        conversations = []
-
-        after = null
+        let conversations = []
+        let after = null
         do {
-            endpoint = this.get_endpoint("messages")
+            let endpoint = this.get_endpoint("messages")
             if (after) { endpoint += `&after=${after}`}
-            newAfter = after
-            await makeAPIRequest(endpoint, sessionStorage.getItem("access-token")).then((batch) => {
+            await makeAPIRequest(endpoint, this.access_token).then((batch) => {
                 console.log(`messages: got ${batch.data.children.length} conversations after ${after}`)
                 batch.data.children.forEach((message) => {
                         message = message.data
-                        authorized_user = this.username
+                        let authorized_user = this.username
                         if (message.subreddit != null) {
                             if (message.distinguished == "moderator" && message.dest != authorized_user) { return }
                             if (message.dest != authorized_user && message.author.name != authorized_user) { return }
@@ -111,14 +109,13 @@ class Reddit {
 
         after = null
         do {
-            endpoint = this.get_endpoint("sent")
+            let endpoint = this.get_endpoint("sent")
             if (after) { endpoint += `&after=${after}`}
-            newAfter = after
-            await makeAPIRequest(endpoint, sessionStorage.getItem("access-token")).then((batch) => {
+            await makeAPIRequest(endpoint, this.access_token).then((batch) => {
                 console.log(`sent: got ${batch.data.children.length} conversations after ${after}`)
                 batch.data.children.forEach((message) => {
                         message = message.data
-                        authorized_user = this.username
+                        let authorized_user = this.username
                         if (message.subreddit != null) {
                             if (message.distinguished == "moderator" && message.dest != authorized_user) { return }
                             if (message.dest != authorized_user && message.author.name != authorized_user) { return }
@@ -126,6 +123,7 @@ class Reddit {
                         if (message.distinguished == "admin" || message.distinguished == "yes") {
                             message.distinguished = "admin"
                         }
+                        if (message.first_message_name) {return}
                         conversations.push(message)
                     })
                 after = batch.data.after
@@ -148,39 +146,8 @@ document.addEventListener('DOMContentLoaded', function() {
     r = new Reddit(redirect_uri,client_id,client_secret,duration,scope)
     r.get_token_from_code().then(()=>{
         document.querySelector('#user_info').innerHTML = `You've now connected YAIR-WEB to Reddit for 
-            <a href="https://reddit.com/user/${r.username}">/u/${r.username}</a>!</div><div><button id="fetchButton" class="button" onclick="r.initialFetch()">Fetch PMs</button>`;
+            <a href="https://reddit.com/user/${r.username}">/u/${r.username}</a>!</div><div><button id="fetchButton" class="button" onclick="initialFetch()">Fetch PMs</button>`;
     })
-    /*
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const code = urlParams.get('code');
-    var body = new URLSearchParams();
-    body.append('grant_type', 'authorization_code');
-    body.append('code', code);
-    body.append('redirect_uri', redirect_uri);
-    fetch(
-        "https://www.reddit.com/api/v1/access_token"
-        , {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-                , 'Authorization': `Basic ${auth_string}`
-            }
-            , body: body
-        }
-    ).then(response => {
-        if (response.ok) { return response.json(); }
-        else { throw new Error('Request failed:', response.status); }
-    }).then(result => {
-        sessionStorage.setItem('access-token', result.access_token);
-        sessionStorage.setItem('refresh-token', result.refresh_token);
-        return makeAPIRequest('api/v1/me', result.access_token);
-    }).then(result => {
-        sessionStorage.setItem('name', result.name)
-        document.querySelector('#user_info').innerHTML = `You've now connected YAIR-WEB to Reddit for 
-            <a href="https://reddit.com/user/${result.name}">/u/${result.name}</a>!</div><div><button id="fetchButton" class="button" onclick="testFetchPMs()">Fetch PMs</button>`;
-    }).catch((error) => { console.error('Error:', error); });
-    */    
 }, false);
 
 function makeAPIRequest(endpoint, token, method = "GET", params = null, body = null) {
@@ -200,43 +167,18 @@ function makeAPIRequest(endpoint, token, method = "GET", params = null, body = n
     }).catch((error) => { console.error('Error:', error); });
 }
 
-async function testFetchPMs() {
+async function InitialFetch() {
     button = document.querySelector("#fetchButton")
     button.disabled = true
     after = null
-    conversations = []
-    do {
-        endpoint = "/message/messages?limit=100"
-        if (after) { endpoint += `&after=${after}`}
-        newAfter = after
-        await makeAPIRequest(endpoint, sessionStorage.getItem("access-token")).then((batch) => {
-            console.log(`got ${batch.data.children.length} conversations after ${after}`)
-            button.innerHTML = `Fetched ${batch.data.children.length} conversations after ${after}`
-            batch.data.children.forEach((message) => {
-                    message = message.data
-                    authorized_user = sessionStorage.getItem("name")
-                    if (message.subreddit != null) {
-                        if (message.distinguished == "moderator" && message.dest != authorized_user) { return }
-                        if (message.dest != authorized_user && message.author.name != authorized_user) { return }
-                    }
-                    //TODO: These should go in a seperate bucket, not be discarded
-                    if (message.distinguished == "admin" || message.distinguished == "yes") {
-                        message.distinguished = "admin"
-                        return
-                    }
-                    conversations.push(message)
-                })
-            after = batch.data.after
-        })
-    } while (after != null);
-
+    await r.initialFetch().then((conversations) => {
     html = ""
-    conversations.forEach((message) =>{
-        if (message.author == sessionStorage.getItem("name")) {
-            other_user = message.dest
-        } else {other_user = message.author}
-        html += `<div><a href=https://reddit.com/message/messages/${message.id}>${message.subject}</a> with ${other_user}</div>`
+        conversations.forEach((message) =>{
+            if (message.author == sessionStorage.getItem("name")) {
+                other_user = message.dest
+            } else {other_user = message.author}
+            html += `<div><a href=https://reddit.com/message/messages/${message.id}>${message.subject}</a> with ${other_user}</div>`
+        })
+        document.querySelector("#user_info").innerHTML=html
     })
-    document.querySelector("#user_info").innerHTML=html
-    return conversations
 }
